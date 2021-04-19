4d36b0f04b948d252817b6d18f7872aec0fc6a4e
rumwerfer <git@hornsteiner.de>
2021-04-19 11:57:48 +0200

    replace sample content

---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Butternut</h1>
+        <h3>simple vegan recipes</h3>  
+      </div>
+    );
+  }
+}
+
+export default Main;
