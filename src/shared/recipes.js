dc42584cdd489336cd544c4d30f9476eaff1ffc8
rumwerfer <git@hornsteiner.de>
2021-04-22 08:08:59 +0200

    recipe data

--- src/shared/recipes.js
+++ src/shared/recipes.js
@@ -1,33 +1,63 @@
 const recipes = [
   {
     id: 0,
-    name: "Burger",
-    tags: ["quick", "and", "dirty"]
+    name: "Seitan Burger",
+    tags: ["quick", "main", "protein"],
+    ingredients: ["bun", "seitan patty", "tomato", "mayonnaise"],
+    directions: ["slice bread", "put stuff inside", "done"],
+    imgAuthor: "Roee Shpernik",
+    imgLicense: "CC BY-SA 4.0",
+    imgSource: "https://commons.wikimedia.org/wiki/File:%D7%94%D7%9E%D7%91%D7%95%D7%A8%D7%92%D7%A8_%D7%98%D7%91%D7%A2%D7%95%D7%A0%D7%99.jpg"
   },
   {
     id: 1,
     name: "Tiramisu",
-    tags: ["quick", "and", "dirty"]
+    tags: ["dessert", "italian"],
+    ingredients: ["zwieback", "coffee", "soy milk", "coconut milk", "pudding"],
+    directions: ["make strong coffee", "make vanilla pudding with soy milk", "mix pudding with coconut milk", "make layers"],
+    imgAuthor: "Raffaele Diomede",
+    imgLicense: "CC BY 2.0",
+    imgSource: "https://commons.wikimedia.org/wiki/File:Tiramisu_-_Raffaele_Diomede.jpg"
   },
   {
     id: 2,
     name: "Yakisoba",
-    tags: ["quick", "and", "dirty"]
+    tags: ["main", "asian", "protein"],
+    ingredients: ["rice noodles", "hoisin sauce", "onion", "carrots", "sesame"],
+    directions: ["fry onion and carrots in a wok", "cook rice noodles for 3 minutes", "add noodles to wok", "add hoisin sauce and sesame"],
+    imgAuthor: "Shinkawana",
+    imgLicense: "CC BY 3.0",
+    imgSource: "https://commons.wikimedia.org/wiki/File:Fujinomiya-yakisoba.jpg"
   },
   {
     id: 3,
     name: "Pizza",
-    tags: ["quick", "and", "italian"]
+    tags: ["main", "italian"],
+    ingredients: ["flour", "yeast", "olive oil", "salt"],
+    directions: ["mix ingredients with lukewarm water", "knead for 10 minutes", "let it rest at a warm place"],
+    imgAuthor: "Lokesh Dhakar",
+    imgLicense: "CC BY 2.0",
+    imgSource: "https://commons.wikimedia.org/wiki/File:Vegan_pizza_at_Pizza_Luc%C3%A9.jpg"
   },
   {
     id: 4,
     name: "Brownies",
-    tags: ["quick", "and", "dirty"]
+    tags: ["dessert"],
+    ingredients: ["flour", "cocoa", "margarine"],
+    directions: ["mix everything", "put in oven", "done"],
+    imgAuthor: "Veganbaking.net",
+    imgLicense: "CC BY-SA 2.0",
+    imgSource: "https://commons.wikimedia.org/wiki/File:Vegan_Chocolate_Fudge.jpg"
   },
   {
     id: 5,
     name: "Spring Rolls",
-    tags: ["quick", "and", "dirty"]
+    tags: ["main", "asian", "quick"],
+    ingredients: ["carrots", "cabbage", "rice paper", "tofu"],
+    directions: ["stir fry veggies and tofu shortly", "moisten rice paper", "put veggies on rice paper and roll", "fry spring roll"],
+    imgAuthor: "Jennifer",
+    imgLicense: "CC BY-SA 2.0",
+    imgSource: "https://commons.wikimedia.org/wiki/File:Vegan_spring_rolls.jpg"
   }
 ];
 
