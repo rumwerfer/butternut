c61569ab3345bc9cc0885143e32578f4aee821e6
rumwerfer <git@hornsteiner.de>
2021-04-24 11:47:29 +0200

    fix router for deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from './components/MainComponent';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Main />
     </BrowserRouter>
   );
